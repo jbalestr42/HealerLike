ffdf1270f9295e064b5e3b65c49fe908a6f75e9b
Julien Balestrieri <julien.balestrieri@gmail.com>
2025-09-13 11:26:12 +0200

    Clean character heal skill + add damage all enemies skill

--- Assets/Scripts/CharacterSkill/ApplyConsumerCharacterSkillFactory.cs
+++ Assets/Scripts/CharacterSkill/ApplyConsumerCharacterSkillFactory.cs
@@ -1,24 +1,24 @@
 using System;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Custom/Data/CharacterSkill/HealCharacterSkill")]
-public class HealCharacterSkillFactory : CharacterSkillFactory<HealCharacterSkill, HealCharacterSkillData> {}
+[CreateAssetMenu(menuName = "Custom/Data/CharacterSkill/ApplyConsumerCharacterSkill")]
+public class ApplyConsumerCharacterSkillFactory : CharacterSkillFactory<ApplyConsumerCharacterSkill, ApplyConsumerCharacterSkillData> {}
 
 [Serializable]
-public class HealCharacterSkillData : BaseCharacterSkillData
+public class ApplyConsumerCharacterSkillData : BaseCharacterSkillData
 {
     [CreateDataButton]
     public AConsumerFactory consumer;
     public float multiplier = 1f;
 }
 
-public class HealCharacterSkill : BaseCharacterSkill<HealCharacterSkillData>
+public class ApplyConsumerCharacterSkill : BaseCharacterSkill<ApplyConsumerCharacterSkillData>
 {
     public override void ApplySkillOnTarget(GameObject source, GameObject target)
     {
         ResourceModifier resourceModifier = new ResourceModifier();
         resourceModifier.consumers.Add(data.consumer.GetConsumer(source, target));
-        resourceModifier.multiplier = -1f * data.multiplier;
+        resourceModifier.multiplier = data.multiplier;
         resourceModifier.source = source;
 
         target.GetComponent<Entity>().health.AddResourceModifier(resourceModifier);
