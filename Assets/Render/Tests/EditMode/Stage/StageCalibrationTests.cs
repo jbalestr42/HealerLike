eb588869af6e3e2ae34b94f3548ee4260e0f0b05
fc <francois.corbel@gmail.com>
2026-09-23 10:16:21 +0000

    Bring Stage tests to Julien's conventions
    BattleFocusTests becomes BattleFocusBoundsTests after the class it tests, and
    StageKeyLightTests leaves the Editor test folder since StageKeyLight is runtime
    code. Dense lines are spread out, fixtures move into SetUp and TearDown.

--- Assets/Render/Tests/EditMode/Stage/StageCalibrationTests.cs
+++ Assets/Render/Tests/EditMode/Stage/StageCalibrationTests.cs
@@ -1,48 +1,71 @@
 using NUnit.Framework;
 using UnityEngine;
+
 namespace HealerLike.Render.Stage
 {
 
 public class StageCalibrationTests
 {
-    [Test]
-    public void FogLeavesForegroundClearAndFarCornerBelowFullFog()
+    GameObject _go;
+    Camera _camera;
+
+    [SetUp]
+    public void SetUp()
     {
-        var board = new Bounds(new Vector3(0,.5f,0), new Vector3(16,0,16));
-        var camera = new Vector3(0,20,-17);
-        var range = StageCalibration.FogRange(camera, board);
-        Assert.That(range.x, Is.EqualTo(Vector3.Distance(camera, board.ClosestPoint(camera))).Within(.001f));
-        Assert.That(range.y, Is.GreaterThan(Vector3.Distance(camera, new Vector3(8,.5f,8))));
+        _go = new GameObject("Camera");
+        _camera = _go.AddComponent<Camera>();
+        _camera.fieldOfView = 40f;
     }
-    [Test]
-    public void HatchScalesWithProjectionAndResolution()
+
+    [TearDown]
+    public void TearDown()
     {
-        var go = new GameObject("Camera");
-        try {
-            var camera = go.AddComponent<Camera>(); camera.fieldOfView = 40;
-            Assert.That(StageCalibration.HatchSpacing(camera, 30,1080), Is.EqualTo(.08088f).Within(.0001));
-            camera.orthographic = true; camera.orthographicSize = 10;
-            Assert.That(StageCalibration.HatchSpacing(camera, 5,1000), Is.EqualTo(.08f).Within(.0001));
-            Assert.That(StageCalibration.HatchSpacing(camera, 50,2000), Is.EqualTo(.04f).Within(.0001));
-        } finally { Object.DestroyImmediate(go); }
+        Object.DestroyImmediate(_go);
     }
+
     [Test]
-    public void PortraitFrameFitsTheBoardWidthAtTheNearEdgeAndPlacesTheCentre()
+    public void FogRange_PortraitCamera_LeavesForegroundClearAndFarCornerBelowFullFog()
     {
-        var go = new GameObject("Camera");
-        try {
-            var camera = go.AddComponent<Camera>(); camera.fieldOfView = 40; camera.aspect = 9f/16f;
-            var board = new Bounds(new Vector3(0,.505f,0), new Vector3(16,0,16));
-            var pose = StageCalibration.Frame(board, 73.7f, 40, 9f/16f, .5f, .42f);
-            go.transform.SetPositionAndRotation(pose.position, pose.rotation);
-            Assert.That(pose.rotation.eulerAngles.x, Is.EqualTo(73.7f).Within(.01f));
-            Assert.That(camera.WorldToViewportPoint(board.center).y, Is.EqualTo(.42f).Within(.002f));
-            var nearLeft = camera.WorldToViewportPoint(new Vector3(-8,.505f,-8)); var nearRight = camera.WorldToViewportPoint(new Vector3(8,.505f,-8));
-            float margin = .5f / 17f; // half a unit of 17 across
-            Assert.That(nearLeft.x, Is.EqualTo(margin).Within(.002f)); Assert.That(nearRight.x, Is.EqualTo(1 - margin).Within(.002f));
-            var far = camera.WorldToViewportPoint(new Vector3(8,.505f,8));
-            Assert.That(far.x, Is.InRange(0f, 1f)); Assert.That(far.y, Is.InRange(0f, 1f));
-        } finally { Object.DestroyImmediate(go); }
+        Bounds board = new Bounds(new Vector3(0f, 0.5f, 0f), new Vector3(16f, 0f, 16f));
+        Vector3 camera = new Vector3(0f, 20f, -17f);
+
+        Vector2 range = StageCalibration.FogRange(camera, board);
+
+        Assert.That(range.x, Is.EqualTo(Vector3.Distance(camera, board.ClosestPoint(camera))).Within(0.001f));
+        Assert.That(range.y, Is.GreaterThan(Vector3.Distance(camera, new Vector3(8f, 0.5f, 8f))));
+    }
+
+    [Test]
+    public void HatchSpacing_PerspectiveAndOrthographic_ScalesWithProjectionAndResolution()
+    {
+        Assert.That(StageCalibration.HatchSpacing(_camera, 30f, 1080), Is.EqualTo(0.08088f).Within(0.0001));
+
+        _camera.orthographic = true;
+        _camera.orthographicSize = 10f;
+
+        Assert.That(StageCalibration.HatchSpacing(_camera, 5f, 1000), Is.EqualTo(0.08f).Within(0.0001));
+        Assert.That(StageCalibration.HatchSpacing(_camera, 50f, 2000), Is.EqualTo(0.04f).Within(0.0001));
+    }
+
+    [Test]
+    public void Frame_PortraitBoard_FitsWidthAtNearEdgeAndPlacesTheCentre()
+    {
+        _camera.aspect = 9f / 16f;
+        Bounds board = new Bounds(new Vector3(0f, 0.505f, 0f), new Vector3(16f, 0f, 16f));
+
+        Pose pose = StageCalibration.Frame(board, 73.7f, 40f, 9f / 16f, 0.5f, 0.42f);
+        _go.transform.SetPositionAndRotation(pose.position, pose.rotation);
+
+        Assert.That(pose.rotation.eulerAngles.x, Is.EqualTo(73.7f).Within(0.01f));
+        Assert.That(_camera.WorldToViewportPoint(board.center).y, Is.EqualTo(0.42f).Within(0.002f));
+        Vector3 nearLeft = _camera.WorldToViewportPoint(new Vector3(-8f, 0.505f, -8f));
+        Vector3 nearRight = _camera.WorldToViewportPoint(new Vector3(8f, 0.505f, -8f));
+        float margin = 0.5f / 17f; // half a unit of 17 across
+        Assert.That(nearLeft.x, Is.EqualTo(margin).Within(0.002f));
+        Assert.That(nearRight.x, Is.EqualTo(1f - margin).Within(0.002f));
+        Vector3 far = _camera.WorldToViewportPoint(new Vector3(8f, 0.505f, 8f));
+        Assert.That(far.x, Is.InRange(0f, 1f));
+        Assert.That(far.y, Is.InRange(0f, 1f));
     }
 }
 
